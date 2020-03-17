16ba34554aa74ca8ce010661c7c04baff74f16e3
tima <tima.ogurtzova@yandex.ru>
2020-03-17 17:29:02 +0300

    try/catch

--- src/main/java/com/hello/dao/UserDaoImpl.java
+++ src/main/java/com/hello/dao/UserDaoImpl.java
@@ -2,11 +2,13 @@ package com.hello.dao;
 
 import com.hello.exception.DBException;
 import com.hello.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.PersistenceContext;
+
 import java.util.List;
 
 @Repository
@@ -21,44 +23,68 @@ public class UserDaoImpl implements UserDao {
     @Override
     public List<User> getAllUser() throws DBException {
         List<User> users;
-        Query<User> query = session.createQuery("SELECT user FROM User user");
-        users = query.list();
+        try {
+            Query<User> query = session.createQuery("SELECT user FROM User user");
+            users = query.list();
+        } catch (HibernateException e) {
+            throw new DBException(e);
+        }
         return users;
     }
 
     @Override
     public User getUserById(long id) throws DBException {
         User user;
-        user = session.find(User.class, id);
+        try {
+            user = session.find(User.class, id);
+        } catch (HibernateException e) {
+            throw new DBException(e);
+        }
         return user;
     }
 
     @Override
     public User getUserWithNameAndPassword(String name, String password) throws DBException {
-        Query query = session.createQuery(
+        User user;
+        try {
+            Query query = session.createQuery(
                     "SELECT u FROM User u WHERE u.name =:nameUser AND u.password =:passwordUser ")
                     .setParameter("nameUser", name)
                     .setParameter("passwordUser", password)
                     .setMaxResults(1);
-            User user = (User) query.uniqueResult();
-
+            user = (User) query.uniqueResult();
+        } catch (HibernateException e) {
+            throw new DBException(e);
+        }
         return user;
     }
 
     @Override
     public void updateUserById(User newParameterUser) throws DBException {
+        try {
             session.update(newParameterUser);
+        } catch (HibernateException e) {
+            throw new DBException(e);
+        }
     }
 
     @Override
     public void addUser(User user) throws DBException {
+        try {
             session.save(user);
+        } catch (HibernateException e) {
+            throw new DBException(e);
+        }
     }
 
     @Override
     public void deleteUserById(long id) throws DBException {
-        Query query = session.createQuery("DELETE FROM User WHERE id =:param");
-        query.setParameter("param", id);
-        query.executeUpdate();
+        try {
+            Query query = session.createQuery("DELETE FROM User WHERE id =:param");
+            query.setParameter("param", id);
+            query.executeUpdate();
+        } catch (HibernateException e) {
+            throw new DBException(e);
+        }
     }
 }
